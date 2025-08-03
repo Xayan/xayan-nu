a6711b47d6aa9b64690fdf2efe5806b56cc3dcae
Damian Stępień <damian.stepien@me.com>
2025-08-03 21:17:54 +0200

    Measure engagement by time

--- assets/js/umami.js
+++ assets/js/umami.js
@@ -5,4 +5,11 @@
   script.src = 'https://cloud.umami.is/script.js';
   script.setAttribute('data-website-id', '066cf2d4-f26a-4f82-9da0-eb3e13c7394d');
   document.head.appendChild(script);
+
+  const engagementTimes = [15, 60, 300];
+  engagementTimes.forEach(seconds => {
+    setTimeout(() => {
+      umami.track(`engaged-${seconds}s`);
+    }, seconds * 1000);
+  });
 })();

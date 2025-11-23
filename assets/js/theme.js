dd9a2dad431152d9966794f135b5db52eb730eb0
Damian Stępień <damian.stepien@me.com>
2025-11-23 01:55:12 +0100

    Styling improvements

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -5,24 +5,17 @@
         // Blur the content when the menu is open
         const cbox = document.getElementById("menu-trigger");
         const area = document.querySelector(".wrapper");
+        const menu = document.querySelector(".menu");
         const body = document.body;
 
+        const toggleMenu = (isOpen) => {
+            body.classList.toggle("nav-open", isOpen);
+            menu.classList.toggle("expanded", isOpen);
+            area.classList.toggle("blurry", isOpen);
+        };
+
         cbox.addEventListener("change", function () {
-            if (this.checked) {
-                const scrollY = window.scrollY;
-                body.style.setProperty("--scroll-y", `${scrollY}px`);
-                body.dataset.scrollY = scrollY;
-
-                body.classList.add("nav-open");
-                area.classList.add("blurry");
-            } else {
-                const scrollY = body.dataset.scrollY || 0;
-
-                area.classList.remove("blurry");
-                body.classList.remove("nav-open");
-                
-                window.scrollTo({ top: parseInt(scrollY), left: 0, behavior: "instant" });
-            }
+            toggleMenu(this.checked);
         });
     });
 })();
